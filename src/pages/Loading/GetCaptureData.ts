a113531ce95d7daee9787ce4e84f1ea61f33491d
ss35789 <ss35789@naver.com>
2022-12-24 20:32:08 +0900

    hashed_value 받기

--- src/pages/Loading/GetCaptureData.ts
+++ src/pages/Loading/GetCaptureData.ts
@@ -1,5 +1,4 @@
 import { backUrl } from '../../variable/url';
-import axios from 'axios';
 
 export const getUtcTime = () => {
   let date = new Date();
@@ -11,30 +10,10 @@ export const getUtcTime = () => {
   console.log(`date: ${date.toISOString()}`);
   return date.toISOString();
 };
-export function makeClean (initialLoadedTime: string, pageLoadedTime: string, pageLeaveTime: string, refererUrl: string): string {
-  const Pathname: string = '37b56a';
-  let HashedValue = '';
-  console.log(Pathname);
-  const getHashedValue = async (first: string, second: string) => {
-    await axios.post(`${backUrl}/s3/find/`, {
-      first_word: { first },
-      second_word: { second }
-    })
-      .then((res) => {
-        HashedValue = res.data;
-      })
-      .catch((e) => console.log(e));
-  };
 
-  if (Pathname.includes('-')) {
-    const Words = Pathname.split('-');
-    void getHashedValue(Words[0], Words[1]);
-  } else {
-    HashedValue = Pathname;
-  }
-
+export function makeClean (initialLoadedTime: string, pageLoadedTime: string, pageLeaveTime: string, refererUrl: string, HashedValue: string): string {
   const bodyContent = {
-    s3: 'https://urls3.kreimben.com/1965dd',
+    s3: 'https://urls3.kreimben.com/1965dd', // window.location
     js_request_time_UTC: initialLoadedTime,
     page_loaded_time: pageLoadedTime,
     page_leave_time: pageLeaveTime,
@@ -49,6 +28,7 @@ export function makeClean (initialLoadedTime: string, pageLoadedTime: string, pa
     },
     body: JSON.stringify(bodyContent)
   };
+
   console.log(`fetch_init: ${JSON.stringify(fetch_init)}`);
   void fetch(`${backUrl}/collect/`,
     fetch_init
@@ -57,6 +37,9 @@ export function makeClean (initialLoadedTime: string, pageLoadedTime: string, pa
     .then(json => {
       console.log(`result json: ${JSON.stringify(json)}`);
       const ws = new WebSocket(`ws://api.urls3.kreimben.com/ws/ad_page/${HashedValue}/`);
+      ws.onerror = function (event) {
+        console.log(event);
+      };
       ws.onopen = function (event) {
         ws.send(JSON.stringify({ captured_data: json.id }));
       };
--- src/pages/Loading/index.tsx
+++ src/pages/Loading/index.tsx
@@ -8,66 +8,48 @@ import { backUrl } from '../../variable/url';
 const Loading = () => {
   const [loading, setLoading] = useState(false);
   const [initialLoadedTime, setInitalLoadedTime] = useState<string>('');
-  const [targetUrl, setTargetUrl] = useState<string>();
+  const [targetUrl, setTargetUrl] = useState<string>('/analytics');
   const [pageLoadedTime, setPageLoadedTime] = useState<string>('');
-  const [hashedValue, setHashedValue] = useState<string>('');
+  const [pageLeaveTime, setPageLeaveTime] = useState<string>('');
+  const [HashedValue, setHashedValue] = useState<string>('');
 
-  let pageLeaveTime: string;
-  const WaitLoading = () => {
-    setTimeout(() => { console.log('Loading Button'); setLoading(true); setPageLoadedTime(getUtcTime()); }, 3000);
-  };
-  useEffect(() => {
-    const referrer = document.referrer;
-    setInitalLoadedTime(getUtcTime());
-    WaitLoading();
-  }, []);
-  window.onload = () => {
-    checkHyphen();
-    console.log(hashedValue);
-  };
-  function checkHyphen () {
-    const url = window.location.href;
-    // const url = 'https://urls3.kreimben.com/charismatic -lie';
-    const hashedValue = url.split('/');
-    const preProcessed: string = hashedValue[hashedValue.length - 1];
-    const params: string[] = preProcessed.split('-');
-    // console.log(params[0], params[1]);
-    // console.log(params.length);
-    if (params.length >= 2) {
-      // console.log(JSON.stringify(getFindValue(params)));
-      getFindValue(params);
+  const GetHashedValue = async () => {
+    const Pathname: string = 'natural-concierge'; // window.location.pathname.substring[1];
+    if (Pathname.includes('-')) {
+      const Words = Pathname.split('-');
+      await axios.post(`${backUrl}/s3/find/`, {
+        first_word: Words[0],
+        second_word: Words[1]
+      })
+        .then((res) => {
+          setHashedValue(res.data.hashed_value);
+        })
+        .catch((e) => console.log(e));
     } else {
-      getS3Value(preProcessed);
+      setHashedValue(Pathname);
     }
   };
 
-  function getS3Value(preProcessed: string) {
-    setHashedValue(preProcessed);
+  const WaitLoading = () => {
+    setTimeout(() => {
+      console.log('Loading Button');
+      setLoading(true); setPageLoadedTime(getUtcTime());
+      setPageLeaveTime(getUtcTime());
+    }, 3000);
   };
-  function getFindValue (params: string[]): any {
-    axios.post(`${backUrl}/s3/find/`, {
-      first_word: params[0],
-      second_word: params[1],
-      headers: {
-        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-        'Content-Type': 'application/json',
-        accept: 'application/json'
-      }
-    }).then((res) => {
-      setHashedValue(JSON.stringify(res.data));
-      return JSON.stringify(res.data);
-    })
-      .catch((err) => console.log(err));
-  };
-  window.onbeforeunload = () => {
-    pageLeaveTime = getUtcTime();
-    console.log('initialLoadedTime: ' + initialLoadedTime + '\n pageLoadedTime: ' + pageLoadedTime + '\n pageLeaveTime: ' + pageLeaveTime);
-    setTargetUrl(makeClean(initialLoadedTime, pageLoadedTime, pageLeaveTime, document.referrer));
+  useEffect(() => {
+    setInitalLoadedTime(getUtcTime());
+    WaitLoading();
+  }, []);
 
-    // FIXME below message.
+  useEffect(() => {
+    if (initialLoadedTime !== '' && pageLoadedTime !== '' && pageLeaveTime !== '') {
+      console.log('initialLoadedTime: ' + initialLoadedTime + '\n pageLoadedTime: ' + pageLoadedTime + '\n pageLeaveTime: ' + pageLeaveTime);
+      void GetHashedValue();
+      setTargetUrl(makeClean(initialLoadedTime, pageLoadedTime, pageLeaveTime, document.referrer, HashedValue));
+    }
+  }, [loading]);
 
-    return 'you are going to out of this page!';
-  };
   useEffect(() => {
   }, [targetUrl]);
   return (
@@ -85,13 +67,17 @@ const Loading = () => {
           {!loading &&
             <h1>Please wait a moment.....</h1>
           }
-          {loading &&
+          {loading && targetUrl !== '/' &&
 
               <h1>Success Load!!</h1>
           }
+          {loading && targetUrl === '/' &&
 
-          <p className="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit
-            the text, and add your own fullscreen background photo to make it your own.</p>
+              <h1>Failed to Load..</h1>
+
+          }
+
+          <h4><p className="lead">If you succeed in loading the page, you can go to the original page, <br></br> but if it fails, you can go to the main page.</p></h4>
 
           {loading &&
               <p className="lead">
