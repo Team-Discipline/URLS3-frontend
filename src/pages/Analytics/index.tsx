dc9d29884d158fff4e5fb0b1cf34ef1499675a07
gka0903 <ham3252@naver.com>
2022-12-29 09:52:06 +0900

    eslint-err

--- src/pages/Analytics/index.tsx
+++ src/pages/Analytics/index.tsx
@@ -1,5 +1,5 @@
 
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import AnalyticsSidebar from '../../components/AnalyticsSidebar';
 import styled from 'styled-components';
 import { Bar } from 'react-chartjs-2';
@@ -7,47 +7,43 @@ import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Toolti
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 const Analytics = () => {
-    const [countries, setCountries] = useState(["1st", "2ed", "3rd", "4th"])
-    // 서버에서 country들의 순위 특정 수까지 가져오기
-    const [countryData, setCountryData] = useState([20, 15, 10, 5])
-    const [chartData, setChartData] = useState({
-        datasets : [],
+  const [countries] = useState(['1st', '2nd', '3rd', '4th']);
+  // 서버에서 country들의 순위 특정 수까지 가져오기
+  const [countryData] = useState([20, 15, 10, 5]);
+  const [chartData, setChartData] = useState({});
+  const [chartOptions, setChartOptions] = useState({});
+
+  useEffect(() => {
+    setChartData({
+      labels: countries,
+      datasets: [
+        {
+          label: 'Member',
+          data: countryData,
+          borderColor: 'rgb(53, 162, 235)',
+          backgroundColor: 'rgb(53, 162, 235, 0.4)'
+        }]
     });
-    const [chartOptions, setChartOptions] = useState({});
-
-    useEffect(() => {
-        setChartData({
-            labels : countries,
-            datasets : [
-                {
-                    label : "Member",
-                    data : countryData,
-                    borderColor : "rgb(53, 162, 235)",
-                    backgroundColor : "rgb(53, 162, 235, 0.4)",
-                },
-            ],
-        });
-        setChartOptions({
-            responsive : true,
-            plugins : {
-                legend : {
-                    position : "top"
-                },
-                title : {
-                    display : true,
-                    text : "Country"
-                }
-            }
-        })
-    })
-
-    return (
+    setChartOptions({
+      responsive: true,
+      plugins: {
+        legend: {
+          position: 'top'
+        },
+        title: {
+          display: true,
+          text: 'Country'
+        }
+      }
+    });
+  }, []);
+  return (
             <Container>
                 <AnalyticsSidebar/>
                 <BarBox>
                     <Bar options={chartOptions} data={chartData}/>
                 </BarBox>
-            </Container>
+                </Container>
 
   );
 };
