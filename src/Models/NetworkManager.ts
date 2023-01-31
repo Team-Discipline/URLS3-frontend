0a5793e65b01495a95626a16cf1ffd39032be499
Aksidion Kreimben <aksidion@kreimben.com>
2023-01-31 16:46:36 +0900

    Refactoring.

---
+++ src/Models/NetworkManager.ts
@@ -0,0 +1,59 @@
+import backUrl from "../variable/url";
+import axios, { AxiosError } from "axios";
+
+
+class NetworkManager {
+
+    static get(
+        accessToken: string,
+        urlDetails: string,
+        completion: (res: any) => void,
+    ) {
+        const defaultHeader = {
+            Authorization: `Bearer ${accessToken}`,
+            accept: "application/json",
+        };
+        const defaultUrl = `${backUrl}/${urlDetails}`;
+
+        axios.get(defaultUrl, { headers: defaultHeader })
+        .then(res => completion(res))
+        .catch(error => NetworkManager.dealWithError(error));
+    }
+
+    static async post(
+        accessToken: string,
+        urlDetails: string,
+        body: object,
+        completion: (res: any) => void,
+    ) {
+        const defaultHeader = {
+            Authorization: `Bearer ${accessToken}`,
+            accept: "application/json",
+        };
+        const defaultUrl = `${backUrl}/${urlDetails}`;
+
+        axios.post(defaultUrl, body, { headers: defaultHeader })
+        .then(res => completion(res))
+        .catch(error => NetworkManager.dealWithError(error));
+    }
+
+    private static dealWithError(error: AxiosError) {
+        // Status code
+        // @ts-ignore
+        if (400 <= error.response && error.response < 500) { // 4xx
+            // @ts-ignore
+            if (error.response == 401) { // Not Authorized.
+                console.error(`status code: ${error.response}, Not Authorized.`);
+                // TODO: 이 에러 뜨면 로그인(인증) 다시 하거나 하는 로직 세워서 구현하기
+            } else {
+                console.error(error);
+            }
+        }
+        // @ts-ignore
+        else if (error.status >= 500) { // 5xx
+            console.error(error);
+        }
+    }
+}
+
+export { NetworkManager };
--- src/components/pages/Main/index.tsx
+++ src/components/pages/Main/index.tsx
@@ -1,135 +1,124 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { useTranslation } from 'react-i18next';
-import axios from 'axios';
-import styled from 'styled-components';
-import { AccessToken } from '../../../variable/token';
-import { backUrl } from '../../../variable/url';
-import { UrlShortForm } from '../../blocks/Main/UrlShortForm';
-import { ShortUrl } from '../../blocks/Main/ShortUrl';
-import { Footer } from '../../blocks/Main/Footer';
+import React, { useCallback, useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
+import axios from "axios";
+import styled from "styled-components";
+import { AccessToken } from "../../../variable/token";
+import backUrl from "../../../variable/url";
+import { UrlShortForm } from "../../blocks/Main/UrlShortForm";
+import { ShortUrl } from "../../blocks/Main/ShortUrl";
+import { Footer } from "../../blocks/Main/Footer";
+import { NetworkManager } from "../../../Models/NetworkManager";
+
 // 버튼 쓸때 여기 참고 https://mui.com/material-ui/react-button/#outlined-buttons
 
 interface S3 {
-  id: string
-  url: string
-  issuer: number
-  s3_url: string
-  target_url: string
-  created_at: string
-  updated_at: string
-  short_by_words: boolean
+    id: string;
+    url: string;
+    issuer: number;
+    s3_url: string;
+    target_url: string;
+    created_at: string;
+    updated_at: string;
+    short_by_words: boolean;
 }
 
 interface Url {
-  [index: string]: string
+    [index: string]: string;
 }
 
 const Main = () => {
-  const { t } = useTranslation();
-  const [url, setUrl] = useState('https://api.urls3.kreimben.com/');
-  const [copyUrl, setCopyUrl] = useState('Make your URL short!');
-  const [copied, setCopied] = useState(false);
-  const [qrVision, setQR] = useState(false);
-  const [urlList, setUrlList] = useState<S3[]>([]);
-  const [urlTrueArr, setUrlTrueArr] = useState<Url>({});
-  const [urlFalseArr, setUrlFalseArr] = useState<Url>({});
-  const [toggle, setToggle] = useState(true);
-  const getUrlList = async () => {
-    await axios.get(`${backUrl}/s3/`, {
-      headers: {
-        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-        Authorization: `Bearer ${AccessToken}`
-      }
-    }).then(r => setUrlList(r.data)).catch(e => console.log(e));
-  };
-  const check = () => {
-    void getUrlList();
-    for (let i = 0; i < urlList.length; i++) {
-      const checkTargetUrl = `${urlList[i].target_url}`;
-      const checkUrl = `${urlList[i].s3_url}`;
-      if (urlList[i].short_by_words) {
-        if (!(checkTargetUrl in urlTrueArr)) {
-          urlTrueArr[checkTargetUrl] = checkUrl;
-          setUrlTrueArr({ ...urlTrueArr, checkTargetURl: checkUrl });
+    const { t } = useTranslation();
+    const [url, setUrl] = useState("https://api.urls3.kreimben.com/");
+    const [copyUrl, setCopyUrl] = useState("Make your URL short!");
+    const [copied, setCopied] = useState(false);
+    const [qrVision, setQR] = useState(false);
+    const [urlList, setUrlList] = useState<S3[]>([]);
+    const [urlTrueArr, setUrlTrueArr] = useState<Url>({});
+    const [urlFalseArr, setUrlFalseArr] = useState<Url>({});
+    const [toggle, setToggle] = useState(true);
+
+    const check = () => {
+        // getUrlList();
+        NetworkManager.get(AccessToken, "/s3/", (res: any) => setUrlList(res.data));
+
+        for (let i = 0; i < urlList.length; i++) {
+            const checkTargetUrl = `${urlList[i].target_url}`;
+            const checkUrl = `${urlList[i].s3_url}`;
+            if (urlList[i].short_by_words) {
+                if (!(checkTargetUrl in urlTrueArr)) {
+                    urlTrueArr[checkTargetUrl] = checkUrl;
+                    setUrlTrueArr({ ...urlTrueArr, checkTargetURl: checkUrl });
+                }
+            } else {
+                if (!(checkTargetUrl in urlFalseArr)) {
+                    urlFalseArr[checkTargetUrl] = checkUrl;
+                    setUrlFalseArr({ ...urlFalseArr, checkTargetURl: checkUrl });
+                }
+            }
         }
-      } else {
-        if (!(checkTargetUrl in urlFalseArr)) {
-          urlFalseArr[checkTargetUrl] = checkUrl;
-          setUrlFalseArr({ ...urlFalseArr, checkTargetURl: checkUrl });
+    };
+    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        check();
+        if (toggle) {
+            if (url in urlTrueArr) {
+                setCopyUrl(urlTrueArr[url]);
+            } else {
+                NetworkManager.post(AccessToken, "s3/", {
+                    target_url: url,
+                    short_by_words: toggle,
+                }, (res => setCopyUrl(res.data.s3_url)));
+            }
+        } else {
+            if (url in urlFalseArr) {
+                setCopyUrl(urlFalseArr[url]);
+            } else {
+                axios.post(`${backUrl}/s3/`, {
+                    target_url: url,
+                    short_by_words: toggle,
+                }, {
+                    withCredentials: true,
+                    headers: {
+                        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+                        Authorization: `Bearer ${AccessToken}`,
+                        "Content-Type": "application/json",
+                        accept: "application/json",
+                    },
+                }).then(json => setCopyUrl(json.data.s3_url)).catch(() => window.alert("에러"));
+            }
         }
-      }
-    }
-  };
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    check();
-    if (toggle) {
-      if (url in urlTrueArr) {
-        setCopyUrl(urlTrueArr[url]);
-      } else {
-        axios.post(`${backUrl}/s3/`, {
-          target_url: url,
-          short_by_words: toggle
-        }, {
-          withCredentials: true,
-          headers: {
-            // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-            Authorization: `Bearer ${AccessToken}`,
-            'Content-Type': 'application/json',
-            accept: 'application/json'
-          }
-        }).then(json => setCopyUrl(json.data.s3_url)).catch(() => window.alert('에러'));
-      }
-    } else {
-      if (url in urlFalseArr) {
-        setCopyUrl(urlFalseArr[url]);
-      } else {
-        axios.post(`${backUrl}/s3/`, {
-          target_url: url,
-          short_by_words: toggle
-        }, {
-          withCredentials: true,
-          headers: {
-            // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-            Authorization: `Bearer ${AccessToken}`,
-            'Content-Type': 'application/json',
-            accept: 'application/json'
-          }
-        }).then(json => setCopyUrl(json.data.s3_url)).catch(() => window.alert('에러'));
-      }
-    }
-  };
-  const copy = async () => {
-    await navigator.clipboard.writeText(copyUrl);
-    setCopied(true);
-    setQR(true);
-    setTimeout(() => {
-      setCopied(false);
-      setQR(false);
-    }, 3000);
-  };
-  const urlHandler = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-    setUrl(e.target.value);
-  }, []);
-  const toggleState = () => setToggle(!toggle);
-  useEffect(() => {
-    void getUrlList();
-  }, []);
-  return (
+    };
+    const copy = async () => {
+        await navigator.clipboard.writeText(copyUrl);
+        setCopied(true);
+        setQR(true);
+        setTimeout(() => {
+            setCopied(false);
+            setQR(false);
+        }, 3000);
+    };
+    const urlHandler = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+        setUrl(e.target.value);
+    }, []);
+    const toggleState = () => setToggle(!toggle);
+    useEffect(() => {
+        void getUrlList();
+    }, []);
+    return (
         <MainContainer>
-            <UrlShortForm onSubmit={onSubmit} urlHandler={urlHandler} toggle={toggle} toggleState={toggleState}/>
-            <ShortUrl copyUrl={copyUrl} copied={copied} copy={copy} translation={t} qrVision={qrVision} url={url}/>
-            <br/><br/>
+            <UrlShortForm onSubmit={onSubmit} urlHandler={urlHandler} toggle={toggle} toggleState={toggleState} />
+            <ShortUrl copyUrl={copyUrl} copied={copied} copy={copy} translation={t} qrVision={qrVision} url={url} />
+            <br /><br />
             <ThirdDiv>
                 Something Here - advertisement
             </ThirdDiv>
-            <Br/>
+            <Br />
             <FourthDiv>
                 Technology - advertisement
             </FourthDiv>
             <Footer url={url} />
         </MainContainer>
-  );
+    );
 };
 const MainContainer = styled.div`
   text-align: center;
